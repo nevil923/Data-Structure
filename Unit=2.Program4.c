9e884ea0d34b64c80235871f58ab75a8badd9d15
NevilSakariya93 <nevil.sakariya140231@marwadiuniversity.ac.in>
2026-08-04 21:03:36 +0530

    Add files via upload

---
+++ Unit=2.Program4.c
@@ -0,0 +1,54 @@
+//enrollment number 92500527120
+// Write a program to find the factorial of a given integer number using stack.
+
+
+#include<stdio.h>
+#define max 30
+
+int stack [max],top=-1;
+void push (int ch);
+int pop();
+
+void main()
+{
+    int i=0,n,fact=1;
+    printf("\n enter value : ");
+    scanf("%d",&n);
+
+    for (i=1;i<=n;i++)
+    {
+        push(i);
+    }
+    while(top!=-1)
+    {
+        fact=fact*pop();
+    }
+    printf("\n factorial is : %d",fact);
+}
+void push(int value)
+{
+    if(top==-1)
+    {
+        printf("\n stack overflow...");
+    }
+    else
+    {
+        top++;
+        stack[top]=value;
+    }
+}
+int pop()
+{
+    int val;
+    if(top==-1)
+    {
+        printf("\n stack is underflow...");
+        return -1;
+    }
+    else
+    {
+        val=stack[top];
+        top--;
+        return val;
+    }
+}
