af872479861f108b14bcf409bb97f93b0cb5f939
NevilSakariya93 <nevil.sakariya140231@marwadiuniversity.ac.in>
2026-08-06 13:22:46 +0530

    Add files via upload

---
+++ Unit=2.Program6.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+
+
+int stack[10];
+int top = -1;
+
+void push(int x)
+{
+    stack[++top] = x;
+}
+
+int pop()
+{
+    return stack[top--];
+}
+
+int main()
+{
+    int a,b,temp;
+
+    printf("Enter first Number: ");
+    scanf("%d", &a);
+
+    printf("Enter second number: ");
+    scanf("%d", &b);
+
+    push(a);
+    push(b);
+
+    b =pop();
+    a =pop();
+
+    while(b!=0)
+    {
+        temp=b;
+        b=a%b;
+        a=temp;
+    }
+    push(a);
+    printf("\n GCD of number is : %d",pop());
+
+}
+
